f6226845a289f74cf54e38bd9565766607bff45c
rishabhjoshi-rj <rishabh.joshi@impactqa.com>
2020-12-03 15:40:19 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import "./App.css";
+import React, { useState } from "react";
 import Pet from "./Pet";
 import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 function App() {
+  const themeHook = useState("darkblue");
   return (
-    <div className="App">
-      <header>
-        <Link to="/">Adopt ME</Link>
-      </header>
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <div className="App">
+          <center>
+            <header>
+              <Link to="/">Adopt ME</Link>
+            </header>
+          </center>
 
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
-    </div>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
   );
 }
 
